89823c7d1c3cd1eb4027c39e62b83c62175cb3fc
Sonu-Kaler <130778161+Sonu-Kaler@users.noreply.github.com>
2023-10-13 17:43:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
 	for(let i=0; i<Str.length; i++){
-	decodedArr+=lookup[Str[i]];
+	decodedArr+=lookup[encodedStr[i]];
 	}
   return decodedArr; //return decodedArr
 }
